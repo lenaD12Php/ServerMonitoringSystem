65008bb3c01072ff2e3ad9981757e696bbc07d4c
Lena Damisi <Lena.damisi12@gmail.com>
2025-10-26 13:18:49 +0200

    add program

---
+++ ServerMonitoringSystem.AlertConsumer/Program.cs
@@ -0,0 +1,51 @@
+﻿//using Microsoft.AspNetCore.Builder;
+//using Microsoft.Extensions.DependencyInjection;
+//using ServerMonitoringSystem.AlertConsumer.Models;
+
+//var builder = WebApplication.CreateBuilder(args);
+
+//builder.Services.AddSignalR();
+//builder.Services.AddCors(options =>
+//{
+//    options.AddPolicy("AllowAll", policy =>
+//        policy.AllowAnyOrigin()
+//              .AllowAnyMethod()
+//              .AllowAnyHeader());
+//});
+
+//var app = builder.Build();
+
+//app.UseCors("AllowAll");
+//app.MapHub<AlertHub>("/alertHub");
+
+//app.Run();
+
+using Microsoft.AspNetCore.SignalR;
+using ServerMonitoringSystem.AlertConsumer.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddSignalR();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
+var app = builder.Build();
+
+app.UseCors("AllowAll");
+app.UseRouting();
+
+app.MapHub<AlertHub>("/alertHub");
+
+app.MapGet("/", () => "Server Monitoring SignalR Hub is running!");
+
+Console.WriteLine("Starting SignalR Hub server");
+
+app.Run("http://localhost:5000");
